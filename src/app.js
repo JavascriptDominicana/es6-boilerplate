85d26ab1e0336faca469336bac8c026f92bc947f
Rene Olivo <minet.ws@gmail.com>
2015-03-28 10:05:59 -0400

    move demo to front page

--- src/app.js
+++
@@ -1,9 +0,0 @@
-import message from 'message';
-
-export class App {
-	constructor() {
-		console.log( 'MESSAGE: ', message );
-	}
-}
-
-var theApp = new App();
--- src/message.js
+++
@@ -1,3 +0,0 @@
-var message = 'Hellow World';
-
-export default message;
